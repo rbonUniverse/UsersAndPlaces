c923b3284965c4baf72bfb1c71b462d48e9535c9
rbonUniverse <101582640+rbonUniverse@users.noreply.github.com>
2023-10-24 04:36:22 +0300

    Update config.ts

--- Backend/util/config.ts
+++ Backend/util/config.ts
@@ -1,10 +1,10 @@
-  class Config {
+class Config {
   public connectionString: string;
   public port: number;
 
   constructor() {
     this.connectionString =
-      "mongodb+srv://<username>:<password>@cluster0.fqvbapr.mongodb.net/UsersAndPlaces?retryWrites=true&w=majority";
+      "mongodb+srv://<USERNAME>:<PASSWORD>@cluster0.fqvbapr.mongodb.net/UsersAndPlaces?retryWrites=true&w=majority";
     this.port = 5001;
   }
 }
